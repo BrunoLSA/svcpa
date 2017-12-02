76fc7733fabd5de89cb8af164c2c826b5c224bbd
Bruno Luiz Santana de Araujo <santanasta@gmail.com>
2017-12-01 22:00:31 -0400

    Corrigindo model Member - campo paid

--- svcpa/core/admin.py
+++ svcpa/core/admin.py
@@ -3,7 +3,7 @@ from svcpa.cadastro.models import Member
 
 
 class MemberModelAdmin(admin.ModelAdmin):
-    list_display = ('name', 'cpf', 'city', 'state', 'created_at')
+    list_display = ('name', 'cpf', 'city', 'state', 'created_at', 'paid')
     date_hierarchy = 'created_at'
     search_fields = ('name', 'email', 'phone', 'cpf', 'neighborhood', 'city', 'state')
     list_filter = ('city',)
