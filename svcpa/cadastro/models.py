bd0292dec047d01181f3206627207bc6a0fb5b19
Bruno Luiz Santana de Araujo <santanasta@gmail.com>
2017-12-01 22:11:38 -0400

    alteração model Member

--- svcpa/cadastro/models.py
+++ svcpa/cadastro/models.py
@@ -15,7 +15,7 @@ class Member(models.Model):
     city = models.CharField('cidade', max_length=20)
     state = models.CharField('estado', max_length=2)
     cep = models.CharField('CEP', max_length=8, validators=[validate_cep], blank=True)
-    created_at = models.DateTimeField('cadastrado em', auto_now_add=True)
+    created_at = models.DateTimeField('cadastrado em', auto_now_add=True, null=True)
     paid = models.DateField('vencimento da anualidade', default=None, null=True, blank=True)
 
     def __str__(self):
