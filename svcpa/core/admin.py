24175eb98cf55f23c3610cef6f52d877ac36379c
Bruno Luiz Santana de Araujo <santanasta@gmail.com>
2017-12-02 06:10:00 -0400

    editado layout do admin form

--- svcpa/core/admin.py
+++ svcpa/core/admin.py
@@ -3,8 +3,21 @@ from svcpa.cadastro.models import Member
 
 
 class MemberModelAdmin(admin.ModelAdmin):
-    list_display = ('name', 'cpf', 'city', 'state', 'paid')
+    list_display = ('name', 'cpf', 'city', 'state', 'since', 'paid')
     search_fields = ('name', 'email', 'phone', 'cpf', 'neighborhood', 'city', 'state')
     list_filter = ('city',)
+    fields = ('name',
+              ('date_of_birth', 'rg', 'cpf'),
+              ('email', 'phone'),
+              ('address', 'neighborhood'),
+              ('city', 'state', 'cep'),
+              'paid')
+
+    def since(self, obj):
+        return '{:02}/{:02}/{:4}'.format(obj.created_at.day,
+                                         obj.created_at.month,
+                                         obj.created_at.year)
+
+    since.short_description = 'cadastrado em'
 
 admin.site.register(Member, MemberModelAdmin)
