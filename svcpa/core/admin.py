779da23fee076219e0b6eeff4661a77ff93f90d4
Bruno Luiz Santana de Araujo <santanasta@gmail.com>
2018-01-08 21:34:56 -0400

    Admin order by name

--- svcpa/core/admin.py
+++ svcpa/core/admin.py
@@ -1,27 +1,6 @@
 from django.contrib import admin
 
-from svcpa.cadastro.models import Member, Payment
 
-
-class PaymentInLine(admin.TabularInline):
-    model = Payment
-    extra = 1
-
-
-class MemberModelAdmin(admin.ModelAdmin):
-    inlines = [PaymentInLine,]
-    list_display = ('name', 'cpf', 'city', 'state', 'member_since', 'payment_due')
-    search_fields = ('name', 'email', 'phone', 'cpf', 'neighborhood', 'city', 'state')
-    list_filter = ('city',)
-    fields = ('name',
-              ('date_of_birth', 'rg', 'cpf'),
-              ('email', 'phone'),
-              ('address', 'neighborhood'),
-              ('city', 'state', 'cep'),
-              ('member_since', 'payment_due'))
-
-
-admin.site.register(Member, MemberModelAdmin)
 admin.site.site_header = 'SVCPA'
 admin.site.index_title = 'SVCPA'
 admin.site.site_title = 'Diretoria SVCPA'
