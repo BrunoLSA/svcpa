6008fa8fbbaeb580eff00b05ca4742ced359b3ba
Bruno Luiz Santana de Araujo <santanasta@gmail.com>
2018-08-07 19:24:23 -0300

    Filtro por anuidade

--- svcpa/cadastro/admin.py
+++ svcpa/cadastro/admin.py
@@ -1,6 +1,26 @@
+from datetime import date
+
 from django.contrib import admin
 
 from svcpa.cadastro.models import Member, Payment
+
+
+class ValidAnunityFilter(admin.SimpleListFilter):
+    '''filtra os cadastros por vencimento da unidade: válida ou vendcida.'''
+    title = ('anuidade')
+    parameter_name = 'anuidade'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('válida', ('Válida')),
+            ('vencida', ('Vencida')),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'válida':
+            return queryset.filter(payment_due__gte=date.today())
+        if self.value() == 'vencida':
+            return queryset.filter(payment_due__lt=date.today())
 
 
 class PaymentInLine(admin.TabularInline):
@@ -12,7 +32,7 @@ class MemberModelAdmin(admin.ModelAdmin):
     inlines = [PaymentInLine,]
     list_display = ('name', 'cpf', 'city', 'state', 'member_since', 'payment_due')
     search_fields = ('name', 'email', 'phone', 'cpf', 'neighborhood', 'city', 'state')
-    list_filter = ('city',)
+    list_filter = ('city', ValidAnunityFilter)
     fields = ('name',
               ('date_of_birth', 'rg', 'cpf'),
               ('email', 'phone'),
